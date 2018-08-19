4df49bb2b8d3fcb8e75b468da6ce1647b9072ef1
Ken <kentodd@frontier.com>
2018-08-19 16:34:51 -0700

    added the facebook addon for lesson 8

---
+++ mysite/myblog/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from myblog.views import stub_view
+from myblog.views import list_view
+from myblog.views import detail_view
+
+urlpatterns = [
+    url(r'^$', list_view, name="blog_index"),
+    url(r'^posts/(\d+)/$', detail_view, name="blog_detail"),
+]
